1e11a6ee350303c811bc29129d4a217faebf680d
Ionut Leustean <ionut.leustean@irian.ro>
2025-04-07 16:09:45 +0300

    labs 2 -  live code

---
+++ angular-apps/projects/lab_2_2/src/app/header/header.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {NgStyle} from '@angular/common';
+
+@Component({
+  selector: 'app-header',
+  imports: [
+    NgStyle
+  ],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+
+}
