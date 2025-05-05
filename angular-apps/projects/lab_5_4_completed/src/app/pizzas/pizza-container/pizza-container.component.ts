7d0e153e13d1248991552e6c6492cb6ec4bba66f
Ionut Leustean <ionut.leustean@irian.ro>
2025-05-05 18:02:52 +0300

    lab_5_4_completed

---
+++ angular-apps/projects/lab_5_4_completed/src/app/pizzas/pizza-container/pizza-container.component.ts
@@ -0,0 +1,53 @@
+import {Component, signal} from '@angular/core';
+import {FeaturePizzaListComponent} from '../feature-pizza-list/feature-pizza-list.component';
+import {PizzaFormComponent} from '../pizza-form/pizza-form.component';
+import {PizzaModel} from '../domain/pizza.model';
+import {PizzaService} from '../pizza.service';
+import {FeatureHottestPizzaComponent} from '../feature-hottest-pizza/feature-hottest-pizza.component';
+
+@Component({
+  selector: 'app-pizza-container',
+  imports: [
+    FeaturePizzaListComponent,
+    PizzaFormComponent,
+    FeatureHottestPizzaComponent
+  ],
+  templateUrl: './pizza-container.component.html',
+  styleUrl: './pizza-container.component.scss'
+})
+export class PizzaContainerComponent {
+
+  constructor(private pizzaService: PizzaService) {
+  }
+
+  get pizzas() {
+    return this.pizzaService.getPizzas()
+  }
+
+  pizza = signal<PizzaModel | null>(null);
+
+  onPizzaChanged($event: PizzaModel) {
+    this.pizzaService.saveOrUpdatePizza($event)
+    this.pizza.set(null);
+  }
+
+  addNewPizza() {
+    this.pizza.set({
+      id: Math.random().toString(),
+      imageUrl: '',
+      name: '',
+      description: '',
+      price: 0,
+      ingredients: [],
+      hotness: 0
+    })
+  }
+
+  onPizzaSelected($event: PizzaModel) {
+    this.pizza.set($event)
+  }
+
+  onPizzaDeleted($event: PizzaModel) {
+    this.pizzaService.deletePizza($event)
+  }
+}
