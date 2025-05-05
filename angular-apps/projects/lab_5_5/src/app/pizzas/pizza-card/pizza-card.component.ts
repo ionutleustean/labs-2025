1e3ee019c412ddaa36f62f39ed6cba4741a725ea
Ionut Leustean <ionut.leustean@irian.ro>
2025-05-05 18:04:54 +0300

    lab_5_5

---
+++ angular-apps/projects/lab_5_5/src/app/pizzas/pizza-card/pizza-card.component.ts
@@ -0,0 +1,16 @@
+import {Component, computed, input} from '@angular/core';
+import {PizzaModel} from '../domain/pizza.model';
+
+@Component({
+  selector: 'app-pizza-card',
+  imports: [],
+  templateUrl: './pizza-card.component.html',
+  styleUrl: './pizza-card.component.scss'
+})
+export class PizzaCardComponent {
+  pizza = input.required<PizzaModel>();
+
+  price = computed( () => {
+    return `${this.pizza().price} RON`;
+  })
+}
